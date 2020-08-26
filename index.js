26f3cf0c65a6c42f51b570747e0958dca51d281c
David Conrad <david.scott.conrad@gmail.com>
2020-08-25 22:47:09 -0400

    Delete Mouse Functionality and randomly generated shapes
    refactor for creating the maze. After testing and demoing. Deleted the random bodies and everything that made mouse interaction work. Also changed height and width to be equal.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const {Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
+const {Engine, Render, Runner, World, Bodies} = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -19,9 +19,7 @@ Render.run(render);
 
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}))
+
 
 //walls
 const walls = [
@@ -33,16 +31,4 @@ const walls = [
 
 World.add(world, walls);
 
-
-//random shapes
-
-for(let i= 0; i< 20; i++){
-    if(Math.random() > 0.5){
-    World.add(world, Bodies.rectangle(Math.random() * width,Math.random() * height,50,50));
-    }
-    else{
-        World.add(world,
-            Bodies.circle(Math.random() * width,Math.random() * height,35))
-    }
-}
 
